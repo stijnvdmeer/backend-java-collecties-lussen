7abe0f050b7b6fac79c6771ee8920e1a2c199790
stijnvdmeer <stijn08072002@outlook.com>
2024-11-27 10:11:28 +0100

    Created numeric and alphabetic arrays in main, created Translator class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code schrijven voor de hoofd-opdracht
+        Integer[] numeric = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        String[] alfabetic = {"nul", "een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen"};
 
 
     }
